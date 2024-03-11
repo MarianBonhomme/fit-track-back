58386ca2d9685b9d4f4049f2b39d9a0e71cdcadb
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-03-11 15:55:00 +0100

    fixtures

--- fixtures/foodConsumptions.fixtures.js
+++ fixtures/foodConsumptions.fixtures.js
@@ -1,33 +1,30 @@
 const foodConsumptionData = [
-  { food_id: 1, quantity: 380, date: '2024-03-09' }, // Riz complet
-  { food_id: 3, quantity: 420, date: '2024-03-09' }, // Viande hachée
-  { food_id: 8, quantity: 18, date: '2024-03-09' },  // Huile d'olive
-  { food_id: 9, quantity: 22, date: '2024-03-09' },  // Huile de colza
-  { food_id: 4, quantity: 5, date: '2024-03-09' },  // Oeuf
+  { food_id: 1, quantity: 2, date: '2024-03-09' }, // Coffee
+  { food_id: 2, quantity: 1, date: '2024-03-09' }, // ZZZ
+  { food_id: 3, quantity: 350, date: '2024-03-09' }, // Riz complet
+  { food_id: 4, quantity: 350, date: '2024-03-09' }, // Viande hachée
+  { food_id: 10, quantity: 20, date: '2024-03-09' },  // Huiles
+  { food_id: 6, quantity: 4, date: '2024-03-09' },   // Oeuf
+  { food_id: 12, quantity: 50, date: '2024-03-09' }, // Flocon d'avoine
+  { food_id: 13, quantity: 30, date: '2024-03-09' }, // Whey
+  { food_id: 11, quantity: 1, date: '2024-03-09' },  // Chocolat Noir
 
-  { food_id: 1, quantity: 390, date: '2024-03-08' }, // Riz complet
-  { food_id: 3, quantity: 410, date: '2024-03-08' }, // Viande hachée
-  { food_id: 8, quantity: 19, date: '2024-03-08' },  // Huile d'olive
-  { food_id: 9, quantity: 21, date: '2024-03-08' },  // Huile de colza
-  { food_id: 4, quantity: 4, date: '2024-03-08' },  // Oeuf
+  { food_id: 1, quantity: 1, date: '2024-03-09' }, // Coffee
+  { food_id: 2, quantity: 2, date: '2024-03-09' }, // ZZZ
+  { food_id: 3, quantity: 400, date: '2024-03-09' }, // Riz complet
+  { food_id: 4, quantity: 400, date: '2024-03-09' }, // Viande hachée
+  { food_id: 10, quantity: 25, date: '2024-03-09' },  // Huiles
+  { food_id: 6, quantity: 3, date: '2024-03-09' },   // Oeuf
+  { food_id: 12, quantity: 30, date: '2024-03-09' }, // Flocon d'avoine
+  { food_id: 13, quantity: 30, date: '2024-03-09' }, // Whey
+  { food_id: 11, quantity: 1, date: '2024-03-09' },  // Chocolat Noir
 
-  { food_id: 1, quantity: 370, date: '2024-03-07' }, // Riz complet
-  { food_id: 3, quantity: 430, date: '2024-03-07' }, // Viande hachée
-  { food_id: 8, quantity: 17, date: '2024-03-07' },  // Huile d'olive
-  { food_id: 9, quantity: 23, date: '2024-03-07' },  // Huile de colza
-  { food_id: 4, quantity: 6, date: '2024-03-07' },  // Oeuf
-
-  { food_id: 1, quantity: 385, date: '2024-03-06' }, // Riz complet
-  { food_id: 3, quantity: 415, date: '2024-03-06' }, // Viande hachée
-  { food_id: 8, quantity: 16, date: '2024-03-06' },  // Huile d'olive
-  { food_id: 9, quantity: 24, date: '2024-03-06' },  // Huile de colza
-  { food_id: 4, quantity: 3, date: '2024-03-06' },  // Oeuf
-
-  { food_id: 1, quantity: 380, date: '2024-03-05' }, // Riz complet
-  { food_id: 3, quantity: 420, date: '2024-03-05' }, // Viande hachée
-  { food_id: 8, quantity: 18, date: '2024-03-05' },  // Huile d'olive
-  { food_id: 9, quantity: 22, date: '2024-03-05' },  // Huile de colza
-  { food_id: 4, quantity: 5, date: '2024-03-05' },  // Oeuf
+  { food_id: 1, quantity: 1, date: '2024-03-09' }, // Coffee
+  { food_id: 3, quantity: 450, date: '2024-03-09' }, // Riz complet
+  { food_id: 4, quantity: 300, date: '2024-03-09' }, // Viande hachée
+  { food_id: 10, quantity: 25, date: '2024-03-09' },  // Huiles
+  { food_id: 13, quantity: 30, date: '2024-03-09' }, // Whey
+  { food_id: 11, quantity: 1, date: '2024-03-09' },  // Chocolat Noir
 ];
 
 module.exports = foodConsumptionData;
--- fixtures/foods.fixtures.js
+++ fixtures/foods.fixtures.js
@@ -1,13 +1,17 @@
 const foods = [
-  { name: 'Riz complet', kcal: 123, prot: 2.6, carb: 25.6, fat: 0.9, unity_id: 1 },
-  { name: 'Riz blanc', kcal: 130, prot: 2.7, carb: 28.7, fat: 0.2, unity_id: 1 },
-  { name: 'Viande hachée (de chez le boucher)', kcal: 250, prot: 26, carb: 0, fat: 17, unity_id: 1 },
-  { name: 'Oeuf', kcal: 68, prot: 5.5, carb: 0.6, fat: 4.8, unity_id: 3 },
-  { name: 'Dinde', kcal: 135, prot: 30, carb: 0, fat: 1, unity_id: 1 },
-  { name: 'Pates complètes', kcal: 180, prot: 8, carb: 39, fat: 1.5, unity_id: 1 },
-  { name: 'Pates blanches', kcal: 175, prot: 5, carb: 35, fat: 1, unity_id: 1 },
-  { name: 'Huile d\'olive', kcal: 120, prot: 0, carb: 0, fat: 14, unity_id: 2 },
-  { name: 'Huile de colza', kcal: 124, prot: 0, carb: 0, fat: 14, unity_id: 2 },
+  { name: 'Coffee', kcal: 0, prot: 0, carb: 0, fat: 0, unity_id: 3 },
+  { name: 'Zzz', kcal: 0, prot: 0, carb: 0, fat: 0, unity_id: 3 },
+  { name: 'Riz', kcal: 357, prot: 8.2, carb: 75, fat: 2.1, unity_id: 1 },
+  { name: 'Viande hachée (Bouché)', kcal: 276, prot: 25, carb: 0, fat: 18, unity_id: 1 },
+  { name: 'Steak Haché Façon Bouchère (Aldi)', kcal: 233, prot: 24.5, carb: 0.5, fat: 15, unity_id: 1 },
+  { name: 'Oeuf', kcal: 145, prot: 12.3, carb: 0.7, fat: 10.3, unity_id: 3 },
+  { name: 'Dinde', kcal: 108, prot: 24, carb: 0, fat: 1.3, unity_id: 1 },
+  { name: 'Filet de Poulet', kcal: 107, prot: 23, carb: 0.5, fat: 1.7, unity_id: 1 },
+  { name: 'Pates', kcal: 347, prot: 13, carb: 64, fat: 2.5, unity_id: 1 },
+  { name: 'Huiles', kcal: 884, prot: 0, carb: 0, fat: 100, unity_id: 2 },
+  { name: 'Chocolat Noir 85%', kcal: 584, prot: 12.5, carb: 19, fat: 46, unity_id: 1 },
+  { name: 'Flocon d\'Avoine', kcal: 357, prot: 13.5, carb: 60, fat: 7.4, unity_id: 1 },
+  { name: 'Whey Native Isolate', kcal: 357, prot: 95, carb: 0.2, fat: 0.4, unity_id: 1 },
 ];
 
 module.exports = foods;
