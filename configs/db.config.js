4685afb4cf16c2217eb391ba4e71ef578771ce91
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-03-25 14:49:30 +0100

    fix(config)

---
+++ configs/db.config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  },
+  // Autres environnements...
+};
+
+module.exports = config;
