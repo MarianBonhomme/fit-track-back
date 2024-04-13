12deb8957828a4539181d75958d683bee6ea6c4a
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-04-13 19:04:00 +0200

    fix(fixtures): naming

--- fixtures/foods.fixtures.js
+++ fixtures/foods.fixtures.js
@@ -5,7 +5,7 @@ const foods = [
   { name: 'Whey Isolate', image: 'uploads/foods/whey.webp', kcal: 371, prot: 80, carb: 10, fat: 1, unity: 'Gram', proportion: 1, is_favorite: 0, is_active: 1, is_manageable: 0 },
   { name: 'Flocon d\'Avoine', image: 'uploads/foods/flocon.jpg', kcal: 372, prot: 14, carb: 59, fat: 7, unity: 'Gram', proportion: 1, is_favorite: 0, is_active: 1, is_manageable: 0 },
   { name: 'Pâtes Complètes', image: 'uploads/foods/pates.jpg', kcal: 337, prot: 11, carb: 65, fat: 2, unity: 'Gram', proportion: 1, is_favorite: 0, is_active: 1, is_manageable: 0 },
-  { name: 'Chocolat Noir 85%', image: 'uploads/foods/chocolat.jpg', kcal: 561, prot: 11, carb: 24, fat: 43, unity: 'Portion', proportion: 10, is_favorite: 0, is_active: 1, is_manageable: 0 },
+  { name: 'Chocolat 85%', image: 'uploads/foods/chocolat.jpg', kcal: 561, prot: 11, carb: 24, fat: 43, unity: 'Portion', proportion: 10, is_favorite: 0, is_active: 1, is_manageable: 0 },
   { name: 'Huiles', image: 'uploads/foods/huiles.jpg', kcal: 900, prot: 0, carb: 0, fat: 100, unity: 'Gram', proportion: 1, is_favorite: 0, is_active: 1, is_manageable: 0 },
   { name: 'Oeuf Bio', image: 'uploads/foods/oeufs.jpg', kcal: 146, prot: 12.5, carb: 0, fat: 10.5, unity: 'Portion', proportion: 60, is_favorite: 0, is_active: 1, is_manageable: 0 },
   { name: 'Café', image: 'uploads/foods/cafe.jpg', kcal: 0, prot: 0, carb: 0, fat: 0, unity: 'Portion', proportion: 1, is_favorite: 0, is_active: 1, is_manageable: 0 },
