e767f927c6594c1336e9f7f2a09d6309eff70a8c
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-04-21 18:58:19 +0200

    fix(fixtures)

--- fixtures/profiles.fixtures.js
+++ fixtures/profiles.fixtures.js
@@ -1,6 +1,6 @@
 const profiles = [
   { pseudo: 'Marian', user_id: 1, avatar_id: 1, color_id: 1 },
-  { pseudo: 'Victoria', user_id: 1, avatar_id: 1, color_id: 1 },
+  { pseudo: 'Victoria', user_id: 1, avatar_id: 4, color_id: 5 },
 ]
 
 module.exports = profiles;
--- fixtures/programs.fixtures.js
+++ fixtures/programs.fixtures.js
@@ -1,17 +1,17 @@
 const programs = [
   { name: '9D1/2/3 - Rings Chin-up', starting_date: '2024-03-20', exercise_id: 2, training_format_id: 4, exercise_variation_id: 1, profile_id: 1, is_finished: 0, is_favorite: 0 },
   { name: '9D1/2/3 - Dips', starting_date: '2024-03-26', exercise_id: 12, training_format_id: 4, exercise_variation_id: 1, profile_id: 1, is_finished: 0, is_favorite: 0 },
-  { name: '6D6 - Bench', starting_date: '2024-03-20', ended_date: '2024-07-04', exercise_id: 13, training_format_id: 3, exercise_variation_id: 1, profile_id: 1, is_finished: 1, is_favorite: 0, finished_reason: 'Arrêt du Bench pour un autre exercice' },
-  { name: '10D1 - OHP', starting_date: '2024-03-26', ended_date: '2024-07-04', exercise_id: 15, training_format_id: 1, exercise_variation_id: 1, profile_id: 1, is_finished: 1, is_favorite: 0, finished_reason: 'Commencé trop haut et incrément trop haut' },
+  { name: '6D6 - Bench', starting_date: '2024-03-20', ended_date: '2024-04-07', exercise_id: 13, training_format_id: 3, exercise_variation_id: 1, profile_id: 1, is_finished: 1, is_favorite: 0, finished_reason: 'Arrêt du Bench pour un autre exercice' },
+  { name: '10D1 - OHP', starting_date: '2024-03-26', ended_date: '2024-04-07', exercise_id: 15, training_format_id: 1, exercise_variation_id: 1, profile_id: 1, is_finished: 1, is_favorite: 0, finished_reason: 'Commencé trop haut et incrément trop haut' },
   { name: '9D1/2/3 - Squat', starting_date: '2024-04-08', exercise_id: 20, training_format_id: 4, exercise_variation_id: 3, profile_id: 1, is_finished: 0, is_favorite: 0 },
 
   { name: '10D1 - Pull-up', starting_date: '2024-03-24', exercise_id: 3, training_format_id: 1, exercise_variation_id: 4, profile_id: 2, is_finished: 0, is_favorite: 0 },
   { name: '9D1/2/3 - Dips', starting_date: '2024-03-24', exercise_id: 12, training_format_id: 4, exercise_variation_id: 1, profile_id: 2, is_finished: 0, is_favorite: 0 },
-  { name: '10D - Pull-up', starting_date: '2024-03-24', ended_date: '2024-13-04', exercise_id: 3, training_format_id: 1, exercise_variation_id: 1, profile_id: 2, is_finished: 1, is_favorite: 0, finished_reason: 'Changement de Format' },
+  { name: '10D - Pull-up', starting_date: '2024-03-24', ended_date: '2024-04-13', exercise_id: 3, training_format_id: 1, exercise_variation_id: 1, profile_id: 2, is_finished: 1, is_favorite: 0, finished_reason: 'Changement de Format' },
   { name: '12D2/3/4 - Pull-up', starting_date: '2024-04-16', exercise_id: 3, training_format_id: 6, exercise_variation_id: 1, profile_id: 2, is_finished: 0, is_favorite: 0 },
   { name: '6D6 - Bench', starting_date: '2024-03-24', ended_date: '2024-09-04', exercise_id: 13, training_format_id: 4, exercise_variation_id: 1, profile_id: 2, is_finished: 1, is_favorite: 0, finished_reason: 'Commencé trop haut et incrément trop haut' },
   { name: '9D1/2/3 - Bench', starting_date: '2024-04-11', exercise_id: 13, training_format_id: 4, exercise_variation_id: 3, profile_id: 2, is_finished: 0, is_favorite: 0 },
-  { name: '9D1/2/3 - Squat', starting_date: '2024-03-26', ended_date: '2024-16-04', exercise_id: 20, training_format_id: 4, exercise_variation_id: 1, profile_id: 2, is_finished: 1, is_favorite: 0, finished_reason: 'Douleur genoux + Prépa Luc Léger' },
+  { name: '9D1/2/3 - Squat', starting_date: '2024-03-26', ended_date: '2024-04-16', exercise_id: 20, training_format_id: 4, exercise_variation_id: 1, profile_id: 2, is_finished: 1, is_favorite: 0, finished_reason: 'Douleur genoux + Prépa Luc Léger' },
 ]
 
 module.exports = programs;
