8c5034d5727419ab1e3ce079b04eb526e780453b
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-05-17 10:45:35 +0200

    optimisation: use compression

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require("cors");
 const app = express();
+const compression = require('compression');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(compression());
 
 // Routes
 app.use('/uploads', express.static('uploads'));
