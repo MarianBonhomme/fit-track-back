eea4cef0f56782b0f560dcbcd77ee910201d0163
Marian Bonhomme <marian.bonhomme@gmail.com>
2024-05-29 13:49:31 +0200

    fix(controller): user signin => return avatar and color

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,13 +5,11 @@ const userController = {
     try {
       const { pseudo, password } = req.body;
 
-      // Vérifier si l'utilisateur existe déjà
       const existingUser = await User.findOne({ where: { pseudo } });
       if (existingUser) {
         return res.status(400).json({ error: 'Cet utilisateur existe déjà.' });
       }
 
-      // Créer un nouvel utilisateur
       const newUser = await User.create({ 
         pseudo, 
         password,
@@ -19,7 +17,21 @@ const userController = {
         color_id: 1,
       });
 
-      res.json(newUser);
+      const userWithDetails = await User.findOne({
+        where: { id: newUser.id },
+        include: [
+          {
+            model: Avatar,
+            as: 'avatar',
+          },
+          {
+            model: Color,
+            as: 'color',
+          }
+        ]
+      });
+
+      res.json(userWithDetails);
     } catch (error) {
       console.error('Erreur lors de l\'inscription :', error);
       res.status(500).json({ error: 'Erreur lors de l\'inscription.' });
@@ -30,18 +42,28 @@ const userController = {
     try {
       const { pseudo, password } = req.body;
 
-      // Trouver l'utilisateur correspondant au pseudo donné
-      const user = await User.findOne({ where: { pseudo } });
+      const user = await User.findOne({ 
+        where: { pseudo },
+        include: [
+          {
+            model: Avatar,
+            as: 'avatar',
+          },
+          {
+            model: Color,
+            as: 'color',
+          }
+        ]
+      });
+
       if (!user) {
         return res.status(404).json({ error: 'Utilisateur non trouvé.' });
       }
 
-      // Vérifier le mot de passe
       if (user.password !== password) {
         return res.status(401).json({ error: 'Mot de passe incorrect.' });
       }
 
-      // Authentification réussie
       res.json(user);
     } catch (error) {
       console.error('Erreur lors de la connexion :', error);
--- fixtures/users.fixtures.js
+++ fixtures/users.fixtures.js
@@ -1,6 +1,6 @@
 const users = [
-  { id: 1, pseudo: 'm', password: 'm', dark_mode: 1},
-  { id: 2, pseudo: 'v', password: 'v', dark_mode: 0},
+  { id: 1, pseudo: 'm', password: 'm', dark_mode: 1, dailyKcal: 3000, dailyProt: 160, dailyFat: 100, dailyCarb: 350, avatar_id: 1, color_id: 10 },
+  { id: 2, pseudo: 'v', password: 'v', dark_mode: 0, dailyKcal: 1800, dailyProt: 110, dailyFat: 70, dailyCarb: 182, avatar_id: 11, color_id: 4 },
 ]
 
 module.exports = users;
